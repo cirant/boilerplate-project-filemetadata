14864ec448bf48c789508698e4369957d9739905
Pablo Cirant <cirant3002@gmail.com>
2019-12-05 23:40:27 -0300

    show img info

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer')
+var upload = multer({
+  dest: 'uploads/'
+})
 
 // require and use "multer"...
 
@@ -11,11 +15,16 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const { file } = req;
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
 });
 
 app.listen(process.env.PORT || 3000, function () {
